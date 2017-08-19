1c131608882216d6ab1a745d14873367afe263e0
Jason Chan <jasonchan35@gmail.com>
2017-08-19 17:58:39 +0200

    added pre-build bin

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "pch.h"
+#include <Shellapi.h>
 
 #define DO_TEST(func) \
 	void func(const wchar_t* filename); \
@@ -13,7 +14,17 @@
 int main() {
 	//const wchar_t* filename = L"C:/Users/Jason/Downloads/CentOS-7-x86_64-Everything-1611.iso";
 	//const wchar_t* filename = L"C:/Users/Jason/Downloads/ubuntu-16.04.2-desktop-amd64.iso";	
-	const wchar_t* filename = L"C:/Users/Jason/Downloads/FreeBSD-11.1-RELEASE-amd64-disc1.iso.xz";	
+	//const wchar_t* filename = L"C:/Users/Jason/Downloads/FreeBSD-11.1-RELEASE-amd64-disc1.iso.xz";	
+
+	int argc;
+	auto** argv = CommandLineToArgvW(GetCommandLine(), &argc);
+
+	if (argc < 2) {
+		printf("LargeFileMD5 <filename>\n");
+		throw Error(LOC);
+	}
+
+	auto* filename = argv[1];
 
 	wprintf(L"MD5 of file: %s\n", filename);
 
